e2379eca04d4bb6d69b1d1272537a774a17d0fbb
mateuszek123321 <klosmateusz534@gmail.com>
2025-12-10 14:19:13 +0100

    init

--- ServerSprintEyeGit/src/main/java/org/example/serversprinteye/repositories/UserRepo.java
+++ ServerSprintEyeGit/src/main/java/org/example/serversprinteye/repositories/UserRepo.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
     Optional<User> findByUserName(String userName);
+
     Optional<User> findByUserEmail(String userEmail);
 
 }
